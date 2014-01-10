74c53a6e2fe1518f97da4dff3a49dc3d532002a7
mrhhug@gmail.com <mrhhug@gmail.com>
2014-01-10 02:29:39 -0500

    commit from script

---
+++ Michael_Hug_cs3540_SPR2014_helloWorld.c
@@ -0,0 +1,15 @@
+/*
+ *Author : Michael Hug
+ *Author email : hmichae4@students.kennesaw.edu
+ *Student of Prof Gayler cs3540 Spr014
+ *Assignment 1 - Southern version of "hello world" program
+ *run like: "gcc ./Michael_Hug_cs3540_SPR2014_helloWorld.c -o helloWorld.out && ./helloWorld.out"
+ */
+  
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Hello ya'll\n");
+	return 0;
+}
