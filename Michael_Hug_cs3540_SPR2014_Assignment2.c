c98613589543890cc2b365b8ae6a98537750adaf
mrhhug <mrhhug@gmail.com>
2014-01-18 16:12:45 -0500

    commit from script

--- Michael_Hug_cs3540_SPR2014_Assignment2.c
+++ Michael_Hug_cs3540_SPR2014_Assignment2.c
@@ -16,188 +16,175 @@
  * postconditions: num_grades points to the number of grades in the array
  * return: non-NULL pointer to an array of grades
  * */
+double* get_grades (int* num_grades);
 
-int* get_grades (int* num_grades);
+/**********************************************************
+ * precondition: grades is not NULL & num_grades >= 0
+ * return: median of elements in array - 0 if array is empty 
+ * */
+double calc_median (double* grades, int num_grades);
+
+/*************************************************
+ * precondition: num_grades >= 0
+ * */
+void display_median (int num_grades, double ave);
 
 /**********************************************************
  * precondition: grades is not NULL & num_grades >= 0
  * return: average of elements in array - 0 if array is empty
  * */
-
-double calc_ave (int* grades, int num_grades);
-
-/*************************************************
- * precondition: num_grades >= 0
- * */
-
-void display_ave (int num_grades, double ave);
-
-/**********************************************************
- * precondition: grades is not NULL & num_grades >= 0
- * return: average of elements in array - 0 if array is empty
- * */
-
-double calc_standard_deviation (int* grades, int num_grades);
+double calc_standard_deviation (double* grades, int num_grades);
 
 /*************************************************
  * precondition: num_grades >= 0
  * */
-
 void display_standard_deviation (int num_grades, double ave);
 
 /**********************************************************
  * precondition: grades is not NULL & num_grades >= 0
- * return: median of elements in array - nothing if array is empty 
- * The no data printf is handled by ave, seems redundant to print again 
+ * return: average of elements in array - 0 if array is empty
  * */
-
-double calc_median (int* grades, int num_grades);
+double calc_ave (double* grades, int num_grades);
 
 /*************************************************
  * precondition: num_grades >= 0
  * */
+void display_ave (int num_grades, double ave);
 
-void display_median (int num_grades, double ave);
+/*************************************************
+ * precondition: grades is not NULL & num_grades >= 0
+ * postcondition : grades is sorted in ascending order
+ * */
+void insertion_sort (double* grades, int num_grades)
 
 int main ()
 {
-  int num_grades = 0;
-  int* grades = get_grades (&num_grades);
-  double ave = calc_ave (grades, num_grades);
-  display_ave (num_grades, ave); 
-  double median = calc_median(grades, num_grades);
-  display_median (num_grades, median);
-  double standard_deviation = calc_standard_deviation(grades, num_grades);
-  display_standard_deviation (num_grades, standard_deviation);
-  return 0;
+	int num_grades = 0;
+	double* grades = get_grades (&num_grades);
+	double ave = calc_ave (grades, num_grades);
+	display_ave (num_grades, ave); 
+	double median = calc_median(grades, num_grades);
+	display_median (num_grades, median);
+	double standard_deviation = calc_standard_deviation(grades, num_grades);
+	display_standard_deviation (num_grades, standard_deviation);
+	return 0;
 }
 
-void insertion_sort (int* grades, int num_grades)
+void insertion_sort (double* grades, int num_grades)
 {
-  int i,j,temp;
-  for(i=1;i<num_grades;i++)
-  {
-      temp = grades[i];
-      j=i-1;
-      while(temp<grades[j] && j>=0)
-      {
-        grades[j+1] = grades[j];
-	--j;
-      }
-      grades[j+1]=temp;
-  }
+	assert (grades != NULL);
+	assert (num_grades >= 0);
+	int i,j,temp;
+	for(i=1;i<num_grades;i++)
+	{
+		temp = grades[i];
+		j=i-1;
+		while(temp<grades[j] && j>=0)
+		{
+			grades[j+1] = grades[j];
+			--j;
+		}
+		grades[j+1]=temp;
+	}
 }
 
 void display_ave (int num_grades, double ave)
 {
-  assert (num_grades >= 0);
-  if (num_grades == 0)
-    printf ("no data\n");
-  else
-    printf ("average: %f\n", ave);
+	assert (num_grades >= 0);
+	if (num_grades == 0)
+		printf ("no data\n");
+	else
+		printf ("average: %g\n", ave);
 }
 
-double calc_ave (int* grades, int num_grades)
+double calc_ave (double* grades, int num_grades)
 {
-  assert (grades != NULL);
-  assert (num_grades >= 0);
-  double ave = 0;
-  if (num_grades > 0)
-  {
-    int sum = 0;
-    int i = 0;
-    for (i = 0; i < num_grades; i++)
-      sum += grades[i];
-    ave = (double)sum / num_grades;
-  }
-  return ave;
+	assert (grades != NULL);
+	assert (num_grades >= 0);
+	double ave = 0;
+	if (num_grades > 0)
+	{
+		int sum = 0;
+		int i = 0;
+		for (i = 0; i < num_grades; i++)
+			sum += grades[i];
+		ave = (double)sum / num_grades;
+	}
+	return ave;
 }
 
 void display_standard_deviation (int num_grades, double standard_deviation)
 {
-  assert (num_grades >= 0);
-  if (num_grades != 0)
-    printf ("standard deviation: %f\n", standard_deviation);
+	assert (num_grades >= 0);
+	if (num_grades != 0)
+		printf ("standard deviation: %g\n", standard_deviation);
 }
 
-double calc_standard_deviation (int* grades, int num_grades)
+double calc_standard_deviation (double* grades, int num_grades)
 {
-  assert (grades != NULL);
-  assert (num_grades >= 0);
-  double standard_deviation, mean = calc_ave(grades, num_grades);
-  int i;
-  for(i = 0; i<num_grades; i++)
-  {
-    grades[i]=(grades[i]-mean);
-    printf("%d\n",grades[i]); 
-  }
-    standard_deviation = (calc_ave(grades, num_grades)); 
-  return standard_deviation;
+	assert (grades != NULL);
+	assert (num_grades >= 0);
+	double standard_deviation = 0;
+	double mean = calc_ave(grades, num_grades);
+	int i;
+	for(i = 0; i<num_grades; i++)
+	{
+		standard_deviation+=(grades[i]-mean)*(grades[i]-mean);
+	}
+	standard_deviation/=num_grades;
+	return standard_deviation;
 }
 
 void display_median (int num_grades, double median)
 {
-  assert (num_grades >= 0);
-  if (num_grades != 0)
-    printf ("median: %f\n", median);
+	assert (num_grades >= 0);
+	if (num_grades != 0)
+		printf ("median: %g\n", median);
 }
 
-double calc_median (int* grades, int num_grades)
+double calc_median (double* grades, int num_grades)
 {
-  assert (grades != NULL);
-  assert (num_grades >= 0);
-  insertion_sort(grades, num_grades);
-  double median;
-  if (num_grades > 0)
-  {
-    if(num_grades % 2==0)
-      median = (grades[num_grades/2] + grades[num_grades/2-1]) / 2.0;
-    else
-      median = grades[num_grades/2];
-  }
-  return median;
+	assert (grades != NULL);
+	assert (num_grades >= 0);
+	double median = 0;
+	if (num_grades > 0)
+	{
+		insertion_sort(grades, num_grades);
+		if(num_grades % 2==0)
+			median = (grades[num_grades/2] + grades[num_grades/2-1]) / 2.0;
+		else
+			median = grades[num_grades/2];
+	}
+	return median;
 }
 
-int* get_grades (int* num_grades)
+double* get_grades (int* num_grades)
 {
-  /*
-  assert (num_grades != NULL);
-  *num_grades = 0;
-  int size = 10;
-  int* grades = malloc (size * sizeof (int));
-  int grade;
-  printf ("enter a grade: \n");
-  scanf ("%d", &grade);
-  while (grade >= 0)
-  {
-    if (*num_grades == size)
-    {
-      int* temp = malloc (2 * size * sizeof (int));
-      int i;
-      for (i = 0; i < size; i++)
-	temp[i] = grades[i];
-      free (grades);
-      grades = temp;
-      size *= 2;
-    }
-    grades[*num_grades] = grade;
-    (*num_grades)++;
-    printf ("enter a grade: \n");
-    scanf ("%d", &grade);
-  }
-  assert (grades != NULL);
-  assert (*num_grades >= 0);
-  return grades;
-  */
-	int* grades = malloc (8 * sizeof (int));
-  grades[0]=45;
-  grades[1]=19;
-  grades[2]=10;
-  grades[3]=99;
-  grades[4]=1;
-  grades[5]=3;
-  grades[6]=4;
-  grades[7]=5;
-  *num_grades=8;
-  return grades; 
+	assert (num_grades != NULL);
+	*num_grades = 0;
+	int size = 10;
+	int* grades = malloc (size * sizeof (int));
+	int grade;
+	printf ("enter a grade: \n");
+	scanf ("%d", &grade);
+	while (grade >= 0)
+	{
+		if (*num_grades == size)
+		{
+			int* temp = malloc (2 * size * sizeof (int));
+			int i;
+			for (i = 0; i < size; i++)
+				temp[i] = grades[i];
+			free (grades);
+			grades = temp;
+			size *= 2;
+		}
+		grades[*num_grades] = grade;
+		(*num_grades)++;
+		printf ("enter a grade: \n");
+		scanf ("%d", &grade);
+	}
+	assert (grades != NULL);
+	assert (*num_grades >= 0);
+	return grades; 
 }
