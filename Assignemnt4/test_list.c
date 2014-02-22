a79682f7db853a91871f21545014fd8d1e2466e9
mrhhug@gmail.com <mrhhug@gmail.com>
2014-02-22 05:31:49 -0500

    commit from script

--- Assignemnt4/test_list.c
+++ Assignemnt4/test_list.c
@@ -2,33 +2,49 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void display_list (list* l);
+void display_list(list* l);
 
-int main ()
+int main()
 {
-  list* l = create_list();
-  append (l, "abc");
-  append (l, "def");
-  prepend (l, "xyz");
-  insert_in_list (l, 1, "mmm");
-  display_list (l);
-  printf ("\n");
-  set (l, 1, "www");
-  display_list(l);
-  return 0;
+	list* l = create_list();
+	append(l, "def");
+	prepend(l, "abc");
+	insert_in_list(l, 1, "mmm");
+	append(l, "ghi");
+	append(l, "jkl");
+	display_list(l);
+	printf("\n");
+	set(l, 0, "www");
+	display_list(l);
+	printf("\n");
+	set(l, 4, "qqq");
+	display_list(l);
+	printf("\n");
+	delete_from_list(l, 4);
+	delete_from_list(l, 0);
+	display_list(l);
+	printf("\n");
+	insert_in_list(l, 1, "abc");
+	insert_in_list(l, 3, "g-.5");
+	display_list(l);
+	printf("\n");
+	set(l, 3, "fff");
+	display_list(l);
+	printf("\n");
+	return 0;
 }
 
-void display_list (list* l)
+void display_list(list* l)
 {
-  if (l == NULL)
-    printf ("null list\n");
-  else
-  {
-    int i;
-    for (i = l -> size; i > 0 ; i--)
-    {
-      char* data = (char*)get (l, i);
-      printf ("%s\n", data);
-    }
-  }
+	if (l == NULL)
+		printf("null list\n");
+	else
+	{
+		int i;
+		for (i = 0; i < l -> size; i++)
+		{
+			char* data = (char*) get(l, i);
+			printf("%s\n", data);
+		}
+	}
 }
