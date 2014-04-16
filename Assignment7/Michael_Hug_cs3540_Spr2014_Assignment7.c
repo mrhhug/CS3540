a09d4eed64ee80601aa6d5c122c140ca06d6b891
mrhhug <mrhhug@gmail.com>
2014-04-16 16:52:59 -0400

    commit from script

--- Assignment7/Michael_Hug_cs3540_Spr2014_Assignment7.c
+++ Assignment7/Michael_Hug_cs3540_Spr2014_Assignment7.c
@@ -2,40 +2,10 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <string.h>
 
 void err_exit (const char* message);
 
-int isFileReadable(const char *fname)
-{
-	int readable =0 ;
-	if(access(fname,R_OK) !=-1)
-	{
-		readable =1;
-	}
-	return readable;
-}
-int isFileWritable(const char *fname)
-{
-	int writable = 0;
-	if(access(fname,W_OK) != -1)
-	{
-		writable = 1;
-	}
-	return writable;
-}
-int doesFileExist(const char *fname)
-{
-	int exists = 0;
-	if (access(fname,F_OK) != -1)
-	{
-		exists = 1;
-	}
-	return exists;
-}
-int createFile(const char *fname)
-{
-	
-}
 void report(char *fname, char *wordreport)
 {
 	if (strcmp(wordreport,"report") != 0)
@@ -45,13 +15,20 @@ void report(char *fname, char *wordreport)
 	if (access(fname,R_OK) !=0)
 		err_exit("unable to read file, check permissions");
 	int fd = open (fname, O_RDONLY, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
-	char buf[5];
 	if (fd < 0)
 		err_exit ("error in opening file for read");
-	int result = read (fd,buf , 5);
-	if (result < 0)
-		err_exit ("write error");
-	printf("%s",buf);
+	int result =1;
+	char namebuf[15];
+	char pricebuf[8];
+	char countbuf[4];
+	while(result>0)
+	{
+		read (fd,namebuf,15);
+		read (fd,pricebuf,8);
+		result = read (fd,countbuf,4);
+		if(result>0 && strcmp(namebuf,"TOMBSTONE!@#$%") )
+			printf("%s\t\t%s X\t%s =\t%f\n",namebuf,pricebuf,countbuf,atof(pricebuf)*atoi(countbuf));
+	}
 	close(fd);
 }
 void err_exit (const char* message)
@@ -62,20 +39,58 @@ void err_exit (const char* message)
 void add(char *fname, char *addKeyword, char *name, char * price, char *count)
 {
 	if (strcmp(addKeyword,"add") != 0)
-		err_exit("Unknown keyword - do you mean 'report'?");
-	if (access(fname,R_OK) !=0)
-		err_exit("unable to write file, check permissions");
+		err_exit("Unknown keyword - do you mean 'add'?");
+	if (atof(price)==0.0)
+		err_exit("the price can not me cast to double");
 	char namebuf[15];
+	if (strlen(name)<15)
+		strcpy(namebuf,name);
 	char pricebuf[8];
+	if(strlen(price)<8)
+		strcpy(pricebuf,price);
 	char countbuf[4];
-	FILE *fp=fopen(fname, "a");
-	fprintf(fp, "%s %s %s", name, price, count);
-	fclose(fp);
+	if(strlen(count)<4)
+		strcpy(countbuf,count);
+	int fd = open(fname, O_RDWR|O_APPEND|O_CREAT,0666);
+	int result = write(fd,namebuf,15);
+	if (result <0)
+		err_exit("could not write name to file");
+	result = write(fd,price,8);
+	if (result <0)
+		err_exit("could not write price to file");
+	result = write(fd,count,4);
+	if (result <0)
+		err_exit("could not write count to file");
+	close(fd);
+}
+void delete(char *fname, char *deleteKeyword, char *name)
+{
+	if (strcmp(deleteKeyword,"delete") != 0)
+		err_exit("Unknown keyword - do you mean 'delete'?");
+	int result =1;
+	char namebuf[15];
+	FILE *fb = fopen(fname,"rb");
+	fseek(fb, 0L, SEEK_END);
+	int sz = ftell(fb);
+	fseek(fb, 0L, SEEK_SET);
+	fclose(fb);
+	printf("%d",sz);
+	int fd = open(fname, O_RDWR);
+	while(result<0)
+	{
+		read (fd,namebuf,15);
+		printf("%s",namebuf);
+		if(strcmp(namebuf,name))
+			write(fd,"TOMBSTONE!@#$%",15);
+		result = lseek(fd,12,SEEK_CUR);
+	}
 }
 int main(int argc, char *argv[])
 {
 	if(argc == 3)
 		report(argv[1],argv[2]);
+	else if(argc == 4)
+		delete(argv[1],argv[2],argv[3]);
 	else if(argc == 6)
 		add(argv[1],argv[2],argv[3],argv[4],argv[5]);
 	else
